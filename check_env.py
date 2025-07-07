c20991ce48ae0c82de90a1d53269c7857e5086d0
EragonBrisingr <geometrydasher26@gmail.com>
2025-07-07 16:08:49 +0200

    tested the env

--- check_env.py
+++ check_env.py
@@ -1,8 +1,18 @@
 from snake_env import SnakeEnv
 from gymnasium.utils.env_checker import check_env
 
-# Instantiate environment
-env = SnakeEnv(render_mode="rgb_array")  # Use rgb_array for headless check
+env = SnakeEnv(render_mode="rgb_array")
 
-# Validate it
+# Validate the environment
 check_env(env, skip_render_check=True)
+
+obs, info = env.reset()
+done = False
+
+while not done:
+    action = env.action_space.sample()
+    obs, reward, done, truncated, info = env.step(action)
+    if env.render_mode == "human":
+        env.render()
+
+env.close()
